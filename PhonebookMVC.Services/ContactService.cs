cf6f989d98883472ce7df4c911cb1a53fcfde73c
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-07-22 19:21:57 +0400

    Connect to sql server.

--- PhonebookMVC.Services/ContactService.cs
+++ PhonebookMVC.Services/ContactService.cs
@@ -1,4 +1,5 @@
 ﻿using PhonebookMVC.Models;
+using System.Data.SqlClient;
 
 namespace PhonebookMVC.Services
 {
@@ -7,15 +8,15 @@ namespace PhonebookMVC.Services
         static List<Contact> _contacts = new List<Contact>();
         public void Create(int count)
         {
-            for (int i = 0; i < count; i++)
-            {
-                _contacts.Add(new Contact
-                {
-                    Company = $"Default Company {i.ToString()}",
-                    Name = $"Default Name {i.ToString()}",
-                    Country = $"Default Country {i.ToString()}"
-                });
-            }
+            //for (int i = 0; i < count; i++)
+            //{
+            //    _contacts.Add(new Contact
+            //    {
+            //        Company = $"Default Company {i.ToString()}",
+            //        Name = $"Default Name {i.ToString()}",
+            //        Country = $"Default Country {i.ToString()}"
+            //    });
+            //}
         }
         public void Create(Contact? contact)
         {
@@ -31,7 +32,37 @@ namespace PhonebookMVC.Services
             => _contacts.FirstOrDefault(x => x.Id == id);
 
         public IEnumerable<Contact>? GetAll()
-            => _contacts;
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Phonebook;Persist Security Info=True;
+                                        User ID=sa;Password=1234";
+            string sqlExpression = "SELECT Contacts.Guid, Companies.Name, Employees.Name, Companies.Country " +
+                                        "FROM((Contacts INNER JOIN Companies ON Contacts.Id_Company = Companies.Id) " +
+                                        "INNER JOIN Employees ON Contacts.Id_Employee = Employees.Id)";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read()) 
+                    {
+                        _contacts.Add(new Contact
+                        {
+                            Id = Guid.Parse(reader.GetValue(0).ToString()),
+                            Company = reader.GetValue(1).ToString(),
+                            Name = reader.GetValue(2).ToString(),
+                            Country = reader.GetValue(3).ToString()
+                            
+                        });
+                    }
+                }
+                reader.Close();
+            }
+            return _contacts;
+        }
+
 
         public void Update(Contact? contact)
         {
